40df7908105e7c8e99c0ed841899e4229098cbe6
Sreenivas7463 <59993636+Sreenivas7463@users.noreply.github.com>
2022-06-13 11:55:24 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ module.exports = {
     locales: ['en'],
     defaultLocale: 'en',
   },
-  images: {
-        //enter the domain or subdomain where you have WordPress installed 
+  images: { //enter the domain or subdomain where you have WordPress installed 
       domains: ['wirally.com'],
     },
     
